a8e1cc84a70046bf5f793488ebe6b2ac4f993ef7
VHile <dalvshutnik@outlook.com>
2022-06-21 08:51:20 +0300

    program modification

--- Qt_Bluetooth/main.py
+++ Qt_Bluetooth/main.py
@@ -3,46 +3,44 @@ from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 from PyQt5.QtCore import QIODevice
 import pandas as pd
 from datetime import datetime
+from collections import deque
+from itertools import islice
 import numpy as np
-from pyqtgraph import PlotWidget
-import pyqtgraph as pg
-import sys
-import pyqtgraph.opengl as gl
+from scipy.fft import rfft, rfftfreq
+
 
 app = QtWidgets.QApplication([])
 ui = uic.loadUi("design.ui")
-ui.setWindowTitle("Serial")
+ui.setWindowTitle("PyGraph")
 
 serial = QSerialPort()
 serial.setBaudRate(115200)
-portlist = []
+port_list = []
 ports = QSerialPortInfo().availablePorts()
 for port in ports:
-    portlist.append(port.portName())
-ui.comL.addItems(portlist)
-print(portlist)
+    port_list.append(port.portName())
+ui.comL.addItems(port_list)
 
-bufferX = np.zeros(1000000, float)
-bufferY = np.zeros(1000000, float)
-bufferZ = np.zeros(1000000, float)
+size = 512
+i = size
 
-listX = []
-listY = []
-listZ = []
-list_t = []
+bufferX = deque([0]*size, maxlen=size)
+bufferY = deque([0]*size, maxlen=size)
+bufferZ = deque([0]*size, maxlen=size)
+bufferTi = deque([0]*size, maxlen=size)
 
-t = 0
-x = 0
-i = 0
+data = []
+dictXYZ = {'X': [], 'Y': [], 'Z': [], 'Time': []}
+
 flag_update = False
 flag_StartEXP = False
 flag_is_start = False
 
-listXYZ = {'X': [], 'Y': [], 'Z': [], 'T': []}
 
 ui.graph.addLegend()
 ui.graph.showGrid(x=True, y=True, alpha=0.8)
-ui.graph.setLabel('left', 'Amplitude (16bit Signed)')
+ui.graph.setLabel('left', 'Amplitude')
+ui.graph.setLabel('bottom', 'Time (ms)')
 
 curve1 = ui.graph.plot(pen="r", name="Accel X")
 curve2 = ui.graph.plot(pen="g", name="Accel Y")
@@ -52,24 +50,17 @@ curve4 = ui.graph.plot()
 
 ui.graph_2.addLegend()
 ui.graph_2.showGrid(x=True, y=True, alpha=0.8)
-ui.graph_2.setLabel('left', 'Amplitude (16bit Signed)')
+ui.graph_2.setLabel('left', 'Amplitude')
+ui.graph_2.setLabel('bottom', 'frq (Hz)')
 
 curve5 = ui.graph_2.plot(pen="r", name="Accel X")
 curve6 = ui.graph_2.plot(pen="g", name="Accel Y")
 curve7 = ui.graph_2.plot(pen="b", name="Accel Z")
 curve8 = ui.graph_2.plot()
 
-#for i in range(len(listX)):
-#    listXYZ['X'].append(listX[i])
-#for i in range(len(listY)):
-#    listXYZ['Y'].append(listY[i])
-#for i in range(len(listZ)):
-#    listXYZ['Z'].append(listZ[i])
 
-data = [None]*5
-
-def onRead():
-    global listX, list_t, t, data, listXYZ
+def on_read():
+    global data
     rx = serial.readLine()
     rxs = str(rx, 'utf-8').strip()
     data = rxs.split('+')
@@ -77,91 +68,113 @@ def onRead():
         ui.lcdX.display(float(data[0]))
         ui.lcdY.display(float(data[1]))
         ui.lcdZ.display(float(data[2]))
-        ui.lcdT.display(float(data[3]))
-    except IndentationError:
-        print('?')
+        ui.lcdT.display(float(data[4]))
+    except IndexError:
+        pass
+
 
 def update():
-    global t, x, listX, listY, listZ,  list_t, flag_StartEXP, data, listXYZ, curve1, curve2, curve3, i, bufferX, bufferY, bufferZ
+    global flag_StartEXP, data, dictXYZ, curve1, curve2, curve3, i, bufferX, bufferY, bufferZ
     if flag_StartEXP:
-        t += 1
-        x += 1
-        listX.append(float(data[0]))
-        listY.append(float(data[1]))
-        listZ.append(float(data[2]))
-        list_t.append(t)
-        bufferX[i] = data[0]
-        bufferY[i] = data[1]
-        bufferZ[i] = data[2]
-        listXYZ['X'].append(float(data[0]))
-        listXYZ['Y'].append(float(data[1]))
-        listXYZ['Z'].append(float(data[2]))
-        listXYZ['T'].append(float(data[3]))
 
-#        ui.graph.clear()
-#        curve1.setData(list_t, listX)
-        curve1.setData(bufferX[0:i])
+        dictXYZ['X'].append(float(data[0]))
+        dictXYZ['Y'].append(float(data[1]))
+        dictXYZ['Z'].append(float(data[2]))
+        dictXYZ['Time'].append(float(data[4]))
+
+        if i > 0:
+            i -= 1
+
+        bufferX.append(float(data[0]))
+        bufferY.append(float(data[1]))
+        bufferZ.append(float(data[2]))
+        bufferTi.append(float(data[4]))
+
+        curve1.setData(list(islice(bufferTi, i, size)), list(islice(bufferX, i, size)))
         curve1.setPos(0, 0)
 
-        curve2.setData(bufferY[0:i])
+        curve2.setData(list(islice(bufferTi, i, size)), list(islice(bufferY, i, size)))
         curve2.setPos(0, 0)
 
-        curve3.setData(bufferZ[0:i])
+        curve3.setData(list(islice(bufferTi, i, size)), list(islice(bufferZ, i, size)))
         curve3.setPos(0, 0)
-        i += 1
+
+        spec_x = rfft(list(islice(bufferX, 0, size)))
+        abs_spec_x = np.abs(spec_x)
+
+        xf = rfftfreq(size, 1. / 100)
+
+        curve5.setData(xf, abs_spec_x)
+        curve5.setPos(0, 0)
+
+        spec_y = rfft(list(islice(bufferY, 0, size)))
+        abs_spec_y = np.abs(spec_y)
+
+        curve6.setData(xf, abs_spec_y)
+        curve6.setPos(0, 0)
+
+        spec_z = rfft(list(islice(bufferZ, 0, size)))
+        abs_spec_z = np.abs(spec_z)
+
+        curve7.setData(xf, abs_spec_z)
+        curve7.setPos(0, 0)
+
         app.processEvents()
 
-def StartEXP():
+
+def start_exp():
     global flag_StartEXP, flag_is_start
     flag_StartEXP = True
     flag_is_start = True
     print("on")
 
-#    serial.write(txs.encode())
 
-def StopEXP():
-    global listXYZ, listX, listY, listZ, flag_StartEXP, n_file, flag_is_start
+def stop_exp():
+    global flag_StartEXP, flag_is_start, dictXYZ, i, bufferX, bufferY, bufferZ,bufferTi, data
     if flag_is_start:
-        df = pd.DataFrame(data=listXYZ)
-
-        new_name_file = str('./Accel/Accel' + str(datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")) + '.xlsx')
-
+        df = pd.DataFrame(data=dictXYZ)
+        new_name_file = str(f'./Accel/Accel{str(datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p"))}.xlsx')
         print(new_name_file)
         df.to_excel(new_name_file)
         print("off")
+
         flag_StartEXP = False
-        listX.clear()
-        listY.clear()
-        listZ.clear()
-        list_t.clear()
 
-        listXYZ['X'].clear()
-        listXYZ['Y'].clear()
-        listXYZ['Z'].clear()
-        listXYZ['T'].clear()
+        bufferX = deque([0] * size, maxlen=size)
+        bufferY = deque([0] * size, maxlen=size)
+        bufferZ = deque([0] * size, maxlen=size)
+        bufferTi = deque([0] * size, maxlen=size)
 
-#        ui.graph.clear()
+        data.clear()
+
+        dictXYZ['X'].clear()
+        dictXYZ['Y'].clear()
+        dictXYZ['Z'].clear()
+        dictXYZ['Time'].clear()
+
+        i = size
+
         flag_is_start = False
-#    serial.write(txs.encode())
 
-def onOpen():
-   serial.setPortName(ui.comL.currentText())
-   serial.open(QIODevice.ReadWrite)
 
-def onClose():
+def on_open():
+    serial.setPortName(ui.comL.currentText())
+    serial.open(QIODevice.ReadWrite)
+
+
+def on_close():
     serial.close()
 
-serial.readyRead.connect(onRead)
-ui.openB.clicked.connect(onOpen)
-ui.closeB.clicked.connect(onClose)
 
-ui.onB.clicked.connect(StartEXP)
-ui.offB.clicked.connect(StopEXP)
+serial.readyRead.connect(on_read)
+ui.openB.clicked.connect(on_open)
+ui.closeB.clicked.connect(on_close)
+
+ui.onB.clicked.connect(start_exp)
+ui.offB.clicked.connect(stop_exp)
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(10)
-#timer.setInterval(dt)
-
 ui.show()
 app.exec()
